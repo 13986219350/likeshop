d2761df8d33514bdc0c093ab15d3d24d19cc04d8
caogang <caogang530@qq.com>
2024-01-11 23:42:07 +0800

    fix sleep

--- start.bat
+++ start.bat
@@ -1,7 +1,11 @@
+docker rm -f likeshop_jenkins
+
 rem Step 1: Git Clone
 git clone git@github.com:13986219350/likeshop.git
 
 rem Step 2: Run Docker Container
 docker run -d --name likeshop_jenkins -p 20209:80 likeshop/php-b2c:2.5.7
 
-timeout /t 600
+sleep 600
+
+docker rm -f likeshop_jenkins
