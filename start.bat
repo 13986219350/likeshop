612f2ec5ece9d9f57f8a7de961b66a649de0df95
caogang <caogang530@qq.com>
2024-01-11 21:42:19 +0800

    add sleep

--- start.bat
+++ start.bat
@@ -3,3 +3,5 @@ git clone git@github.com:13986219350/likeshop.git
 
 rem Step 2: Run Docker Container
 docker run -d --name likeshop_jenkins -p 20209:80 likeshop/php-b2c:2.5.7
+
+timeout /t 600 /nobreak
